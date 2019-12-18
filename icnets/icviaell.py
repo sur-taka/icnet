144f471af0744678fb64591754e09608775cf2a5
fabian <fheil0815@googlemail.com>
2019-12-18 12:59:09 +0100

    test

--- icnets/icviaell.py
+++ icnets/icviaell.py
@@ -1,6 +1,7 @@
 import scipy as sp
 import numpy as np
 import scipy.special as ssp
+import numpy.linalg as la
 
 def ell(u,m):
 	return ssp.ellipj(u,m)
@@ -12,7 +13,7 @@ def dn(u,m):
 	return ell(u,m)[2]
 
 def mkl(a,b,k,s,st):
-	m=1-(a**2)/(b**2)
+	m=1-(b**2)/(a**2)
 	n=[]
 	d=[]
 	for i in range(0,k):
@@ -22,21 +23,31 @@ def mkl(a,b,k,s,st):
 
 	return [n,d]
 
-mcfn(n,d)
-M=[[,-1],[,-1],[,-1]]
-MM=np.array(M)
-
+def mcfn(n,d):
+    M=[[n[0,0],n[0,1],-1],[n[1,0],n[1,1],-1],[n[2,0],n[2,1],-1]]
+    MM=np.array(M)
+    print(MM)
+    MI = la.inv(MM)
+    print(d)
+    r = np.matmul(MI,d)
+    return [[r[0],r[1]], r[2]]
 
 a = int(input("Alpha: "))
 b = int(input("Beta: "))
 k = int(input("K: "))
 s = int(input("S1: "))
 
-[nl,dl]=mkl(a,b,k,0,0.2)
-[nm,dm]=mkl(a,b,k,s,0.2)
+[nl,dl]=mkl(a,b,k,0,0.25)
+[nm,dm]=mkl(a,b,k,s,0.25)
 print(nl)
 print(dl)
 print(nm)
 print(dm)
+nn = np.array([nl[0],nl[1],nm[0]])
+dd = np.array([[dl[0]],[dl[1]],[dm[0]]])
+[m,r]=mcfn(nn,dd)
+print(m)
+print(r)
+
 
 		
